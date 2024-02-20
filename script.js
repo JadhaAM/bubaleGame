d0d375e4771bc0e05675b92e3acd39e73c05fe98
JadhaAM <134080298+JadhaAM@users.noreply.github.com>
2024-02-20 22:57:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,8 +71,8 @@ document.querySelector("#pbtm")
     .addEventListener("click", (detail) => {
         let clickNumber = Number(detail.target.textContent);
         if (hitrn === clickNumber) {
-            incresSocre();
             makeBubale();
+            incresSocre();
             getNewHit();
         }
     })
